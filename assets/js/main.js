b445c62867b87ef97c153ada6202fdd08c790a68
groveld <3328493+groveld@users.noreply.github.com>
2025-07-24 12:42:51 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,6 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("/serviceworker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
   })
 }
 {% endif %}
